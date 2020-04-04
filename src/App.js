8f07cc266608b65f2133cd188100cb52f813be80
Serhii Luhovyi (serhii.luhovyi) <serhii.luhovyil@gmail.com>
2020-04-04 10:56:29 +0300

    Fetch users from github api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({loading: true});
+
+    const res = await axios.get(
+        `https://api.github.com/users?q=
+      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    console.log(res.data)
+
+    this.setState({users: res.data, loading: false});
+  }
+
+  render() {
+    return (
+        <div className='App'>
+          <Navbar/>
+          <div className='container'>
+            <Users loading={this.state.loading} users={this.state.users}/>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
