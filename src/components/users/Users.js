10f6fa5e7ec01603ca8e566065094f14b763af81
Serhii Luhovyi (serhii.luhovyi) <serhii.luhovyil@gmail.com>
2020-04-04 15:07:51 +0300

    Base functional

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -4,17 +4,17 @@ import Spinner from '../layout/Spinner';
 import PropTypes from  'prop-types';
 
 const Users = ({ users, loading }) => {
-    if(loading) {
-      return <Spinner />
-    } else {
-      return (
+  if(loading) {
+    return <Spinner />
+  } else {
+    return (
         <div style={userStyle}>
           {users.map(user => (
-            <UserItem key={user.id} user={user} />
+              <UserItem key={user.id} user={user} />
           ))}
         </div>
-      );
-    }
+    );
+  }
 }
 
 Users.propTypes = {
